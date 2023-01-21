1e64ae3d859ae604c34bd242522cde5ca39d16f0
Meerim <turdalivameerim63@gmail.com>
2023-01-21 18:42:20 +0600

    done

---
+++ app.js
@@ -0,0 +1,8 @@
+// const first = document.querySelector('#frirst')
+// const second = document.querySelector('#second')
+// const random = document.querySelector('button')
+
+// const randOne=Math.round(Math.random()*100)
+// const randTwo=Math.round(Math.random()*100)
+
+
